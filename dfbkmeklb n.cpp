7f774e4715f8fa9159978c4e9b93cd8100ea8464
leonarderx <92988489+leonarderx@users.noreply.github.com>
2022-10-23 19:05:20 +0300

    Add files via upload

---
+++ dfbkmeklb n.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+//1
+
+//int main() {
+    //setlocale(LC_ALL, "Russian");
+    //float  res = 0, r;
+    //int i;
+   // for (i = 1; i <= 10; ++i) {
+    //    cout << "i" << i << "-";
+    //    cin >> r;
+     //   res += r;
+  //  }
+   // cout << res;
+   // return 0;
+//}
+
+//2
+
+//#include <iostream>
+//using namespace std;
+
+//int main() {
+    //setlocale(LC_ALL, "Russian");
+   // float  rez = 1, r;
+    //int i;
+    //for (i = 1; i <= 10; ++i) {
+     //   cout << "i" << i << "-";
+     //   cin >> r;
+    //    rez *= r;
+  //  }
+//    cout << rez;
+  //  return 0;
+//} 
+
+//3
+
+//#include <iostream>
+//using namespace std;
+
+//int main() {
+   // setlocale(LC_ALL, "Russian");
+   //float  etog = 1, r;
+  //  int i;
+   // for (i = 1; i <= 10; ++i) {
+    //    cout << "i" << i << "-";
+     //   cin >> r;
+     //   etog += r;
+
+    //}
+  // etog = etog / 10;
+  //  cout << etog;
+    //return 0;    
+
+
+
+
+
+
+
+/}
